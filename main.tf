add92ad76f14b759a565a4969e5bf020e63ced5c
shakirhms12 <2020a1r019@mietjammu.in>
2025-01-07 16:30:04 +0530

    add

--- main.tf
+++ main.tf
@@ -12,32 +12,6 @@ resource "aws_instance" "windows" {
   subnet_id = aws_subnet.subnet_aws.id
   vpc_security_group_ids = [aws_security_group.security_group.id]
   key_name = "my_aws_key"
-
-   # Upload PowerShell script
-  provisioner "file" {
-    source      = templatefile("${path.module}/windows-setup.ps1.tpl", {
-      vm_name         = var.vm_name
-      install_software = var.install_software
-    })
-    destination = "C:\\setup\\windows-setup.ps1"
-
-     connection {
-      type     = "winrm"
-      host     = aws_instance.windows.public_ip
-      user     = "Administrator"
-      password = var.password  # Use a secure method to retrieve this password
-      https    = false
-      insecure = true
-      port     = 5985
-    }
-  }
-
-  # Execute PowerShell script
-  provisioner "remote-exec" {
-    inline = [
-      "powershell.exe -ExecutionPolicy Bypass -File C:\\setup\\windows-setup.ps1"
-    ]
-  }
   # Add tags to the instance
   tags = {
     Name        =  "windows"                               # "webserver-${terraform.workspace}"
@@ -55,5 +29,45 @@ resource "aws_instance" "windows" {
       iam_instance_profile
     ]
   }
-  
+
+}
+
+resource "null_resource" "setup_windows_iis" {
+  triggers = {
+    instance_ip = aws_instance.windows.public_ip
+  }
+
+  provisioner "file" {
+    source      = templatefile("${path.module}/windows-setup.ps1.tpl", {
+      vm_name         = var.vm_name
+      install_software = var.install_software
+    })
+    destination = "C:\\setup\\windows-setup.ps1"
+
+    connection {
+      type     = "winrm"
+      host     = aws_instance.windows.public_ip  # Using the public IP of the instance
+      user     = "Administrator"
+      password = var.password  # Use a secure method to retrieve this password
+      https    = false
+      insecure = true
+      port     = 5985
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "powershell.exe -ExecutionPolicy Bypass -File C:\\setup\\windows-setup.ps1"
+    ]
+
+    connection {
+      type     = "winrm"
+      host     = aws_instance.windows.public_ip  # Using the public IP of the instance
+      user     = "Administrator"
+      password = var.password  # Use a secure method to retrieve this password
+      https    = false
+      insecure = true
+      port     = 5985
+    }
+  }
 }
